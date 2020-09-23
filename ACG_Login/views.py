e7251b0e708519d205d12cff3f8b58428d73ecb4
ErminLA <42754356+ErminLA@users.noreply.github.com>
2020-09-23 18:21:37 +0200

    Update views.py

--- ACG_Login/views.py
+++ ACG_Login/views.py
@@ -4,13 +4,43 @@ from django.http import HttpResponse
 import hashlib
 import urllib.request 
 import json
+from django.template.context_processors import request
 
 def home(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def get_hashed_vault_creds():
-    url = "https://erminkreponic1c.mylabserver.com/v1/kv-test/test03-kv"
-    hdr = { 'X-Vault-Token' : 's.9IzOHb4ybMhUfHeTxmGMrWsd' }
+    url = "https://erminkreponic1c.mylabserver.com/v1/our-auth/creds"
+    hdr = { 'X-Vault-Token' : 's.eKAC2vSW0rhGb8HXFwvWSjRY' }
+    
+    req = urllib.request.Request(url, headers=hdr)
+    response = urllib.request.urlopen(req)
+    my_json = json.loads(response.read()) #.decode('utf8')
+    creds = []
+    for key, value in my_json['data'].items():
+        creds.append(key)
+        creds.append(value)
+        
+
+    # return hashlib.sha256((creds[0] + creds[1]).encode()).hexdigest()
+    return creds 
+    
+
+def acg_login_view(request, *args, **kwargs):
+    
+    credsHash = hashlib.sha256((request.POST['InputEmail'] + request.POST['InputPassword']).encode()).hexdigest()
+    print("credsHash: ", credsHash)
+    print("vaultHash: ", get_hashed_vault_creds()[1])
+    
+    if get_hashed_vault_creds()[1] == credsHash:
+        return HttpResponse('Authorized', status=200)
+    else:
+        return HttpResponse('Unauthorized', status=401)
+    
+    
+def get_token():
+    url = "https://erminkreponic1c.mylabserver.com/v1/api-keys/api-01"
+    hdr = { 'X-Vault-Token' : 's.svodbkb0WV3GyIEHqIXez6AG' }
     
     req = urllib.request.Request(url, headers=hdr)
     response = urllib.request.urlopen(req)
@@ -20,15 +50,13 @@ def get_hashed_vault_creds():
         creds.append(key)
         creds.append(value)
 
-    return hashlib.sha256((creds[0] + creds[1]).encode()).hexdigest()
+    return value 
 
-def acg_login_view(request, *args, **kwargs):
+def api_auth_test(request, *args, **kwargs):
+    print("REQUEST: ", request.headers['Auth-Token'])
+    print("GET_TOKEN: ", get_token())
     
-    credsHash = hashlib.sha256((request.POST['InputEmail'] + request.POST['InputPassword']).encode()).hexdigest()
-    if get_hashed_vault_creds() == credsHash:
-        return HttpResponse('Authorized', status=200)
+    if request.headers['Auth-Token'] == get_token():
+        return HttpResponse("IT WORKS")
     else:
         return HttpResponse('Unauthorized', status=401)
-         
-
-
